7a3dd96825f2213e8f6c0cf5d4164493c38c947b
Mursalin Kabir <mail2mursalin@gmail.com>
2015-05-16 14:26:24 +0600

    HTML Validator updated

--- htmlvalidator.py
+++ htmlvalidator.py
@@ -1,9 +1,22 @@
 import urllib.request
-
+import requests
+from bs4 import BeautifulSoup
 url = "http://validator.w3.org/check?uri="
-siteurl="http://www.heppnetz.de"
+siteurl="www.northsouth.edu"
 mainurl=url+siteurl
 response =  urllib.request.urlopen(mainurl)
-server=response.getheader('x-w3c-validator-status')
-print(server)
 
+status=response.getheader('x-w3c-validator-status')
+errors=response.getheader('X-W3C-Validator-Errors')
+warnings=response.getheader('X-W3C-Validator-Warnings')
+print("HTML is %s"%(status))
+print("HTML has %s Errors"%(errors))
+print("HTML has %s Warnings"%(warnings))
+#getting line numbers
+r = requests.get(mainurl)
+html= r.content
+soup = BeautifulSoup(html)
+g_data = soup.find_all("em")
+for itm in g_data:
+    lines=itm.text
+    print("There is Error in %s" %(lines))
